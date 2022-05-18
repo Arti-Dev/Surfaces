d0273d135c2ae0e7a0dabf08016a7878d15eee84
Arti-Dev <46334223+Arti-Dev@users.noreply.github.com>
2022-05-18 13:10:04 -0400

    Record TaskID

--- src/main/java/com/articreep/surfaces/surfaces/SlipperySurface.java
+++ src/main/java/com/articreep/surfaces/surfaces/SlipperySurface.java
@@ -12,15 +12,19 @@ public class SlipperySurface extends Surface {
     public SlipperySurface(Block block, BlockFace face) {
         super(block, face);
     }
-
-    @Override
+    
     public void run() {
         Particle.DustOptions dust = new Particle.DustOptions(Color.WHITE, 1);
-        for (Location l : particleLocations) {
-            if (Math.random() < 0.01) {
-                center.getWorld().spawnParticle(Particle.SNOWFLAKE, l, 1, 0, 0, 0, 0);
+        BukkitRunnable task = new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (Location l : particleLocations) {
+                    if (Math.random() < 0.01) {
+                    center.getWorld().spawnParticle(Particle.SNOWFLAKE, l, 1, 0, 0, 0, 0);
+                }
+                center.getWorld().spawnParticle(Particle.REDSTONE, l, 1, 0, 0, 0, 0, dust);
             }
-            center.getWorld().spawnParticle(Particle.REDSTONE, l, 1, 0, 0, 0, 0, dust);
-        }
+        }.runTaskTimer(Main.getPlugin(), 0, 3);
+        taskID = task.getTaskId();
     }
 }
