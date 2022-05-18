c54e6f46bc19b24d2f8f1963fe2adab301c35f01
Arti-Dev <46334223+Arti-Dev@users.noreply.github.com>
2022-05-18 13:33:03 -0400

    Create wrapper class for my block&blockface key
    Missing imports, will do them when I'm back on my IDE

---
+++ src/main/java/com/articreep/surfaces/surfaces/SpecificBlockFace.java
@@ -0,0 +1,26 @@
+package com.articreep.surfaces.surfaces;
+
+public class SpecificBlockFace {
+  
+  private Block block;
+  private BlockFace face;
+  
+  public SpecificBlockFace(Block block, BlockFace face) {
+    this.block = block;
+    this.face = face;
+  }
+  
+  @Override
+  public equals(Object o) {
+    if (o instanceof SpecificBlockFace) {
+      return (block.equals(o.getBlock) && face.equals(o.getFace));
+    }
+    return false;
+  }
+  
+  public Block getBlock() {
+    return block;
+  }
+  
+  public BlockFace getFace() {
+    return face;
